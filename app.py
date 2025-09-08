41776ff0d7de8c20645382793d08fbf1a57c456b
Andres Verona <saqv2002@gmail.com>
2025-09-07 22:16:26 -0500

    primera ventana creada, es un avance

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import tkinter 
-
+import tkinter as tk
 class Main(tk.Tk):
     def __init__(self):
-        
+        super().__init__()
+        self.title("Ventana principal")
+        self.geometry("300x200")
+        tk.Label(self, text= "Esta es la ventana principal").pack(pady=10)
+
+
+if __name__=="__main__":
+    app = Main()
+    app.mainloop()
