1c1199ab4e4a8211f2ec9e04f5914fc5f9483a05
Andres Verona <saqv2002@gmail.com>
2025-09-11 00:46:03 -0500

    ya añadí las tres ventanas con botones funcionales, solo faltarían los labels para ingresar la información y la logica para manipular los datos

--- app.py
+++ app.py
@@ -3,20 +3,40 @@ class Main(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("LOGIN ADMIN")
-        self.geometry("300x200")
+        self.geometry("300x300")
         tk.Label(self, text= "Esta es la ventana principal").pack(pady=10)
-        boton1 = tk.Button(self, text="Entrar", cursor="hand2", bg="#00f4fc", width=10, relief="flat", 
-                              font=("Roboto", 13))# command=lambda: ventana2())
+        boton1 = tk.Button(self, text="Entrar", cursor="hand2", bg="#00f4fc", width=10, relief="flat", command=self.abrirVentana2)
         boton1.place(x=200, y=100) 
-        Contraseña = tk.Entry(self, textvariable=)
+
+        #en esta línea pediremos al usuario ingresar la contraseña
+        Contraseña = tk.Entry(self)
         Contraseña.pack
         Contraseña.get()
-class Ventana2(tk.Toplevel):
-    def ventana2(self):
-        self.title("LOGIN ADMIN")
-        self.geometry("300x200")
-        tk.Label(self, text= "Esta es la ventana secundaria").pack(pady=10)
+        
+    def abrirVentana2(self):
+        Ventana2(self)
 
+class Ventana2(tk.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("LOGIN ADMIN")
+        self.geometry("500x300")
+        tk.Label(self, text= "Esta es la ventana secundaria").pack(pady=100)
+        boton1 = tk.Button(self, text="Entrar", cursor="hand2", bg="#00f4fc", width=10, relief="flat", command=self.abrirVentana3)
+        boton1.place(x=200, y=100) 
+    def abrirVentana3(self):
+        Ventana3(self)
+
+
+
+class Ventana3(tk.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("LOGIN ADMIN")
+        self.geometry("500x300")
+        tk.Label(self, text= "Esta es la ventana secundaria").pack(pady=100)
+        boton1 = tk.Button(self, text="salir", cursor="hand2", bg="#00f4fc", width=10, relief="flat", command=exit)
+        boton1.place(x=200, y=100) 
 
 if __name__=="__main__":
     app = Main()
