9dac2830f45864954b526175b7570dde3007c420
Andres Verona <saqv2002@gmail.com>
2025-09-11 00:19:02 -0500

    empezando de nuevo

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import tkinter as tk
 class Main(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Ventana principal")
+        self.title("LOGIN ADMIN")
         self.geometry("300x200")
         tk.Label(self, text= "Esta es la ventana principal").pack(pady=10)
+        boton1 = tk.Button(self, text="Entrar", cursor="hand2", bg="#00f4fc", width=10, relief="flat", 
+                              font=("Roboto", 13))# command=lambda: ventana2())
+        boton1.place(x=200, y=100) 
+        Contraseña = tk.Entry(self, textvariable=)
+        Contraseña.pack
+        Contraseña.get()
+class Ventana2(tk.Toplevel):
+    def ventana2(self):
+        self.title("LOGIN ADMIN")
+        self.geometry("300x200")
+        tk.Label(self, text= "Esta es la ventana secundaria").pack(pady=10)
 
 
 if __name__=="__main__":
